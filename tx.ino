15f499d269256221afc6a42129dfeaa81aaf782d
Aatman Patel <106943496+aatmanp4@users.noreply.github.com>
2025-07-19 11:24:16 -0400

    Create tx.ino

---
+++ tx.ino
@@ -0,0 +1,18 @@
+#include <SPI.h>
+#include <Adafruit_AD9833.h>
+
+#define FSYNC_PIN 10
+Adafruit_AD9833 ad9833 = Adafruit_AD9833(FSYNC_PIN);
+
+void setup() {
+  Serial.begin(115200);
+  ad9833.begin();
+  ad9833.setWaveForm(AD9833_SINE);
+  ad9833.setFrequency(AD9833_FREQ0, 180000);  // 180 kHz
+  ad9833.enableOutput(true);
+  Serial.println("Transmitting 180 kHz sine wave via capacitive plate...");
+}
+
+void loop() {
+  
+}
