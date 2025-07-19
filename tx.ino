a1466c03388f6a88f1b4180a2b98352bd7a002b2
Aatman Patel <106943496+aatmanp4@users.noreply.github.com>
2025-07-19 11:25:56 -0400

    Update tx.ino

--- tx.ino
+++ tx.ino
@@ -8,11 +8,11 @@ void setup() {
   Serial.begin(115200);
   ad9833.begin();
   ad9833.setWaveForm(AD9833_SINE);
-  ad9833.setFrequency(AD9833_FREQ0, 180000);  // 180 kHz
+  ad9833.setFrequency(AD9833_FREQ0, 180000); // Set to 180 kHz
   ad9833.enableOutput(true);
-  Serial.println("Transmitting 180 kHz sine wave via capacitive plate...");
+  Serial.println("Transmitting 180 kHz sine wave...");
 }
 
 void loop() {
-  
+  // signal output is continuous so leave this blank
 }
